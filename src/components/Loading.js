9d4c240ef6d955d7dc5e2e0e17295a4d689c7254
pinghae <pinghae@gmail.com>
2022-07-23 10:17:15 +0700

    done

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="d-flex justify-content-center align-items-center">
+            <div class="spinner-border text-primary text-white" role="status"></div>
+        </div>
+    );
+};
+
+export default Loading
